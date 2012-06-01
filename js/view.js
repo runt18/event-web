555f7638a205c3aeff01df2dc4f2499e62ed3504
Giles Lavelle <giles.lavelle@gmail.com>
2012-06-01 14:53:54 +0100

    Restructured using Backbone LayoutManager plugin

--- js/view.js
+++ js/view.js
@@ -4,19 +4,7 @@ Giles Lavelle
 
 (function($){
 
-var colours = [
-    'red',
-    'red',
-    'red',
-    'red',
-    'yellow',
-    'yellow',
-    'yellow',
-    'green',
-    'green',
-    'green',
-    'green'
-];
+var colours = 'red red red red yellow yellow yellow green green green green'.split(' ');
 
 var PossibleTime = Backbone.Model.extend({
     defaults: {
@@ -24,90 +12,52 @@ var PossibleTime = Backbone.Model.extend({
         duration: 60,
         date: '12/12/12',
         confirmed: 14,
-        total: 17,
-        attendees: [
-            'bob', 'tim'
-        ]
+        total: 17
     }
 });
+
+var MainDetails = Backbone.Model.extend({
+    defaults: {
+        name: 'Lan Party',
+        location: "Oli's House",
+        description: "lorem ipsum"
+    }
+});
+
 
 var PossibleTimes = Backbone.Collection.extend({
     model: PossibleTime
 });
 
 var TimeView = Backbone.View.extend({
-    tagName: 'div',
-    className: 'time-wrap',
-    template: $('#time').html(),
-
-    render: function(){
-        var tmpl = _.template(this.template);
-
-        this.$el.html(tmpl(this.model.toJSON()));
-        return this;
-    }
-});
-
-var AttendeesView = Backbone.View.extend({
-    tagName: 'ul',
-    className: 'attendees',
-    template: $('#attendees').html(),
-
-
-    render: function(){
-        var tmpl = _.template(this.template);
-
-        this.$el.html(tmpl(this.model.toJSON()));
-        return this;
-    }
-});
-
-
-var GlobalAttendeesView = Backbone.View.extend({
-    tagName: 'ul',
-    className: 'global-attendees',
-    template: $('#global-attendees').html(),
-    model: new Backbone.Model.extend({
-        name: 'bob'
-    }),
-
-    render: function(){
-        var tmpl = _.template(this.template);
-
-        this.$el.html(tmpl(this.model.toJSON()));
-        return this;
-    }
-});
-
-//var a = new GlobalAttendeesView().render();
-
-
-var TimesListView = Backbone.View.extend({
-    el: $('#time-list'),
-
+    template: '#time',
     initialize: function(){
-        this.collection = new PossibleTimes([{}]);
-        this.render();
+        this.serialize = this.model.toJSON();
+        this.model.bind("change", this.render, this);
     },
 
-    render: function(){
-        var that = this;
-        _.each(this.collection.models, function(item){
-            that.renderTime(item);
-        }, this);
+    events: {
+        'click input.tick': 'update'
     },
-
-    renderTime: function (item) {
-        var timeView = new TimeView({
-            model: item
-        });
-        this.$el.append(timeView.render().el);
+    update: function(){
+        log('hi');
+        this.model.set('confirmed', 5);
     }
 });
 
-var timesList = new TimesListView();
+var Expander = Backbone.View.extend({
+    template: '#expander',
+    events: {
+        'click': 'expand'
+    },
+    expand: function(){
+        this.$el.find('.expander').toggleClass('expander-closed');
+    }
+});
 
 var PieChartView = Backbone.View.extend({
+    template: '#piechart',
+
     colour: function(){
         var index = Math.floor(this.ratio * 10);
         var colour = colours[index];
@@ -116,7 +66,7 @@ var PieChartView = Backbone.View.extend({
     },
 
     draw: function(){
-        var canvas = $('canvas')[this.index];
+        var canvas = this.$el.find('canvas')[0];
         var ctx = canvas.getContext('2d');
 
         var size = canvas.height;
@@ -139,28 +89,153 @@ var PieChartView = Backbone.View.extend({
         ctx.fill();
     },
 
+    render: function(manage) {
+        var a = manage(this).render();
+        this.draw();
+        return a;
+    },
+
     update: function(ratio){
         this.ratio = ratio;
         this.draw();
     },
 
-    initialize: function(ratio, index){
-        this.index = index;
+    initialize: function(ratio){
         this.ratio = ratio;
-        this.draw();
     }
 });
 
-var c = new PieChartView(0.3, 0);
+var TimesListView = Backbone.View.extend({
+    template: '#times-tmpl',
+
+    initialize: function(){
+        this.collection = new PossibleTimes([{}]);
+    },
+
+    render: function(manage){
+        var view = manage(this);
+        this.collection.each(function(model){
+            view.insert('div', new TimeView({
+                model: model,
+                views: {
+                    '.piechart-wrap': new PieChartView(0.5),
+                    '.expander-wrap': new Expander()
+                }
+            }));
+        });
+
+        return view.render();
+    }
+});
+
+var DetailsView = Backbone.View.extend({
+    template: '#details',
+    model: new MainDetails(),
+    serialize: function(){
+        return this.model.toJSON();
+    }
+});
+
+var Attendee = Backbone.Model.extend({
+    defaults: {
+        name: 'bob'
+    }
+});
+
+var Attendees = Backbone.Collection.extend({
+    model: Attendee
+});
+
+var AttendeesView = Backbone.View.extend({
+    template: '#attendees'
+});
+
+var AttendeeView = Backbone.View.extend({
+    template: '#attendee',
+    tagName: 'li',
+    serialize: function(){
+        return this.model.toJSON();
+    }
+});
+
+var GlobalAttendeesView = Backbone.View.extend({
+    template: '#global-attendees',
+
+    initialize: function(){
+        this.collection = new Attendees([{},{name: 'tim'}]);
+    },
+
+    render: function(manage){
+        var view = manage(this);
+        this.collection.each(function(model){
+            view.insert(new AttendeeView({
+                model: model
+            }));
+        });
+
+        return view.render();
+    }
+});
+
+var ChatView = Backbone.View.extend({
+    template: '#chat'
+});
+
+var main = new Backbone.LayoutManager({
+    template: '#main-layout',
+
+    views: {
+        '#main-details': new DetailsView(),
+        '#global-attendees-list': new GlobalAttendeesView(),
+        '#time-wrap': new TimesListView(),
+        '#chat-view': new ChatView()
+    }
+});
+
+main.$el.appendTo('body');
+main.render();
+
+
+
+// var AttendeesView = Backbone.View.extend({
+//     tagName: 'ul',
+//     className: 'attendees',
+//     template: $('#attendees').html(),
+
+//     render: function(){
+//         var tmpl = _.template(this.template);
+
+//         this.$el.html(tmpl(this.model.toJSON()));
+//         return this;
+//     }
+// });
+
+
+
+// var GlobalAttendeesView = Backbone.View.extend({
+//     tagName: 'li',
+//     className: 'global-attendees',
+//     template: $('#global-attendees').html(),
+//     model: new Backbone.Model.extend({
+//         name: 'bob'
+//     }),
+
+//     render: function(){
+//         var tmpl = _.template(this.template);
+
+//         this.$el.html(tmpl(this.model.toJSON()));
+//         return this;
+//     }
+// });
+
+//var b = new GlobalAttendees();
+//var a = new GlobalAttendeesView({model:b.model}).render();
+
+//var timesList = new TimesListView();
 
 }(jQuery));
 
-/*App.Title = Em.View.extend({
-    name: 'Lan Party',
-    location: "Oli's House",
-    description: "lorem ipsum"
-});
-
+/*
 App.Invitees = Em.View.extend({
     invitees: [
         'Bob',
