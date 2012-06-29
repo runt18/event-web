95276a510219e8e73270bfb53e73709bba72eb84
Giles Lavelle <giles.lavelle@gmail.com>
2012-06-29 19:43:16 +0100

    indentation

--- jasmine/spec/event.spec.js
+++ jasmine/spec/event.spec.js
@@ -1,7 +1,9 @@
 require(['event'], function(Event){
     describe("A Backbone model representing the entire event", function() {
         it("should have a property called total that is the same as the length of the array of invitees", function() {
-            expect(new Event({invitees:[{},{},{}]}).get('total')).toBe(3);
+            expect(new Event({
+                invitees:[{}, {}, {}]
+            }).get('total')).toBe(3);
         });
     });
 });
